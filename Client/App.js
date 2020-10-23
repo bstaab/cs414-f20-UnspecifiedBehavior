15da983bdb0e6e54ce1a475cda985563394fa595
mmihevc <mmihevc@rams.colostate.edu>
2020-10-22 19:10:19 -0600

    redesigned the UI

--- Client/App.js
+++ Client/App.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-//import Navbar from './components/Navbar'
 
 import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
 import Board from './pages/Board'
+import LoginLayout from "./pages/loginlayout";
+
+import useWindowSize from "./components/useWindowSize";
 
 function App() {
+    const {width, height} = useWindowSize();
+    const [adminDashboard, setAdminDashboard] = useState(false);
+
     return (
         <BrowserRouter>
             <div id='body' className="container mt-2" style={{ marginTop: 40}}>
             <Switch>
                 <Route exact path="/">
-                    <Login />
+                    <LoginLayout width={width} height={height}>
+                        <Login setAdminDashboard={setAdminDashboard} />
+                    </LoginLayout>
                 </Route>
                 <Route  path="/Home">
                     <Home />
                 </Route>
                 <Route path="/Register">
-                    <Register />
+                    <LoginLayout width={width} height={height} >
+                        <Register />
+                    </LoginLayout>
                 </Route>
                 <Route path="/Board">
                     <Board />
