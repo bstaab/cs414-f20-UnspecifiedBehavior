8620fe487a9fd4e7ebf7dc64fa330e8accf609ed
mmihevc <mmihevc@rams.colostate.edu>
2020-11-30 20:31:15 -0700

    can kind of send a login request

--- Game/src/main/java/com/mrpowers/requests/Login.java
+++ Game/src/main/java/com/mrpowers/requests/Login.java
@@ -6,6 +6,7 @@ public class Login extends RequestData{
     private String username;
     private String password;
 
+
     public Login(String username, String password){
         this.username=username;
         this.password=password;
