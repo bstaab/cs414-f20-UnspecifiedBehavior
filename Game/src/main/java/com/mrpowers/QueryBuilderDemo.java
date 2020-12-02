d904c161189d76a2cb5e1a4c557924d5eafb760b
keabethg <keabethg@rams.colostate.edu>
2020-12-01 18:43:08 -0700

    added messages table and game state table

--- Game/src/main/java/com/mrpowers/QueryBuilderDemo.java
+++ Game/src/main/java/com/mrpowers/QueryBuilderDemo.java
@@ -2,11 +2,24 @@ package com.mrpowers;
 
 public class QueryBuilderDemo {
     public static void main(String[] args){
-        QueryBuilder.connectDb();
-        QueryBuilder.getDBTable();
-        QueryBuilder.createUser("testuser","test@user","test123");
-        String [] s = QueryBuilder.searchUser("testuser");
-        QueryBuilder.changePassword("testuser","weeeeeee");
-        QueryBuilder.disconnectDb();
+//        QueryBuilder.connectDb();
+//        QueryBuilder.getDBTable();
+//        QueryBuilder.getStateTable();
+//        QueryBuilder.getMessagesTable();
+//        QueryBuilder.addMessage("fella","lass","hey there");
+//        QueryBuilder.removeMessage("fella","lass","hey there");
+//        String s = QueryBuilder.getMessage("user1","user2");
+//        String s = QueryBuilder.getState("user1","user2");
+//        QueryBuilder.addGameState("user1","user2","woooooooooBlahhhhh");
+//        QueryBuilder.updateState("codeuser","testuser","state is changed again");
+//        int [] i = QueryBuilder.getMatches("testuser");
+//        System.out.println("Matches:"+ i[0] + " Matches Won:" + i[1]);
+//        QueryBuilder.updateMatches("testuser",false);
+//        int [] r = QueryBuilder.getMatches("testuser");
+//        System.out.println("Matches:"+ r[0] + " Matches Won:" + r[1]);
+//        QueryBuilder.createUser("tuser","test@ruser","123test");
+//        String [] s = QueryBuilder.searchUser("testuser");
+//        QueryBuilder.changePassword("testuser","wuuuuuuuuuu");
+//        QueryBuilder.disconnectDb();
     }
 }
