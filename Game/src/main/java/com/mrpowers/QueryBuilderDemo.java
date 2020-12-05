44f38b8f1ba9848c6ad2ae038007d397f0d4eba2
kgriem <kgriem@rams.colostate.edu>
2020-12-05 14:29:40 -0700

    db list size get users

--- Game/src/main/java/com/mrpowers/QueryBuilderDemo.java
+++ Game/src/main/java/com/mrpowers/QueryBuilderDemo.java
@@ -2,24 +2,25 @@ package com.mrpowers;
 
 public class QueryBuilderDemo {
     public static void main(String[] args){
-/*      QueryBuilder.connectDb();
-        QueryBuilder.getDBTable();
-        QueryBuilder.getStateTable();
+        QueryBuilder.connectDb();
+//      QueryBuilder.getDBTable();
+//      QueryBuilder.getStateTable();
         QueryBuilder.getMessagesTable();
-        QueryBuilder.addMessage("fella","lass","hey there");
-        QueryBuilder.removeMessage("fella","lass","hey there");
+        int c = QueryBuilder.getListSize("user1");
+        String [] usernames = QueryBuilder.getUsernamesFrom("user1");
+        for(int i = 0; i < c; i++) {
+            System.out.println(usernames[i]);
+        }
+//      QueryBuilder.addMessage("fella","lass","hey there");
+//      QueryBuilder.removeMessage("fella","lass","hey there");
+        /*
         String s = QueryBuilder.getMessage("user1","user2");
         String s = QueryBuilder.getState("user1","user2");
         QueryBuilder.addGameState("user1","user2","woooooooooBlahhhhh");
-        QueryBuilder.updateState("codeuser","testuser","state is changed again");
-        int [] i = QueryBuilder.getMatches("testuser");
-        System.out.println("Matches:"+ i[0] + " Matches Won:" + i[1]);
-        QueryBuilder.updateMatches("testuser",false);
-        int [] r = QueryBuilder.getMatches("testuser");
         System.out.println("Matches:"+ r[0] + " Matches Won:" + r[1]);
         QueryBuilder.createUser("tuser","test@ruser","123test");
         String [] s = QueryBuilder.searchUser("testuser");
-        QueryBuilder.changePassword("testuser","wuuuuuuuuuu");
-        QueryBuilder.disconnectDb();*/
+        QueryBuilder.changePassword("testuser","wuuuuuuuuuu");*/
+        QueryBuilder.disconnectDb();
     }
 }
